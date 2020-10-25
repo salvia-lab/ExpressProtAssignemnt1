196ccfef5ee3c58a8451bf2b1afb5629bee98bcd
salvia-lab <salviarahman1@gmail.com>
2020-10-24 23:47:57 -0400

    updated DB URI for working with Mongo DB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
- "URI": "mongodb://localhost/business_contact"
+ //"URI": "mongodb://localhost/business_contact"
+ "URI":"mongodb+srv://SalviaRahman:p0UPugDd3Xy54tAq@mongodbserver.kauov.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
